040ab8dd5f2e9f4d44e1b3ffcd9110df29f0dd85
Dustin Aguilar <dustin@dustinaguilar.com>
2014-02-18 00:19:00 -0500

    Initial Commit
    Dropped files into the repo.

---
+++ free_settings.php
@@ -0,0 +1,7 @@
+<?php
+
+$email_to = "eightball007@gmail.com"; // your email address
+$email_subject = "Message sent from Portfolio Website"; // email subject line
+$thankyou = "thankyou.htm"; // thank you page
+
+?>
