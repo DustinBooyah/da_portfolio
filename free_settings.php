0d1368acdd9d3baf5f9a406f8f8cd2a5794b891c
Dustin Aguilar <dustin@dustinaguilar.com>
2015-04-15 01:52:02 -0400

    Dropping in the new hotness
    Redid much of the code for the new site. Almost there, I need new
    artwork so I'm just waiting on that.

--- free_settings.php
+++ free_settings.php
@@ -1,6 +1,6 @@
 <?php
 
-$email_to = "eightball007@gmail.com"; // your email address
+$email_to = "eightball007@hotmail.com"; // your email address
 $email_subject = "Message sent from Portfolio Website"; // email subject line
 $thankyou = "thankyou.htm"; // thank you page
 
