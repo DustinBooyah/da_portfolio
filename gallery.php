d7a1d33134d5d1af926e2e557b29d45ecbdfc005
Dustin Aguilar <dustin@dustinaguilar.com>
2015-05-18 22:52:38 -0400

    Almost finished!

--- gallery.php
+++ gallery.php
@@ -50,61 +50,61 @@
     </nav>
   </header>
 
-	<section>
-		<div class="row small-only-text-center">
+    <section>
+        <div class="row small-only-text-center">
       <h1 class="head_inner">Gallery</h1>
         <div class="large-12 column">
           <div id="galleria">
             <a href="img/images_gallery/b-22.png" id="fullscreen">
-            <img src="img/images_gallery/22.png" title="Brinkman Surveying<br><small>Adobe CS6 (Illustrator, Photoshop, Fireworks) Notepad ++, Firebug</small>" alt="This project called for a good looking web presence that would allow Brinkman Surveying's customers to request estimates online. <br><br> I used large photos and text to illustrate what they do. The background of the inner pages is an actual survey map.
+            <img src="img/images_gallery/22.png" title="Brinkman Surveying<br><small>Adobe CS6 (Illustrator, Photoshop, Fireworks) Notepad ++, Firebug</small>" alt="Brinkman Surveying wanted a good looking web presence that allows their customers to communicate with them online. The site is streamlined and mobile-friendly.<br><small>brinkmansurveying.com</small>
             "></a>
             <a href="img/images_gallery/b-02.png" id="fullscreen">
-            <img src="img/images_gallery/02.png" title="Mock - Nu Skool In School for 352<br><small>Adobe Fireworks CS5, Adobe Photoshop CS5</small>" alt="During my internship at 352, our final design project was to create our own imaginary charity and design a homepage for it. This charity is for keeping teachers and students engaged with music in school.
+            <img src="img/images_gallery/02.png" title="Mock - Nu Skool In School<br><small>Fireworks CS5, Photoshop CS5</small>" alt="One of my tasks at 352 was to create an imaginary charity and design a homepage for it. I came up with a charity that keeps teachers and students engaged with music via electronic components such as analog synthesizers.
             "></a>
             <a href="img/images_gallery/b-21.png" id="fullscreen">
-            <img src="img/images_gallery/21.png" title="OwnForce<br><small>Adobe CS6 (Illustrator, Photoshop, Fireworks) Ruby on Rails</small>" alt="<p>OwnForce is an online contracting platform. I designed the initial logo, mockups and graphics implemented throughout.I also did stationary in Illustrator and Photoshop.<br/><br/>The description of the logo can be seen in the photograph, while examples of the stationary can be found on Behance.</p>
+            <img src="img/images_gallery/21.png" title="OwnForce<br><small>Adobe CS6 (Illustrator, Photoshop, Fireworks) Ruby on Rails</small>" alt="I designed the initial logo, mockups and graphics implemented throughout. I also did stationary in Illustrator and Photoshop, examples of which can be found on Behance.<br><small>ownforce.com</small>
             "></a>
             <a href="img/images_gallery/b-20.png" id="fullscreen">
-            <img src="img/images_gallery/20.png" title="Same Day Appliance Repair<br><small></small>" alt="http://www.samedayrepairgainesville.com<br/>Notepad++, Firebug for Firefox, Adobe Fireworks CS6, Adobe Photoshop CS6<br/><br/>Same Day Appliance Repair wanted a website that looked friendly and professional, provided tips for appliance maintenance and allowed online inquiries. I provided a layout plan, heavily refined the content and designed with a stainless steel refrigerator in mind. I coded using HTML5 and CSS3 and designed in Photoshop. It's due for an update in 2015.
+            <img src="img/images_gallery/20.png" title="Same Day Appliance Repair<br><small>Notepad++, Firebug, Fireworks CS6, Photoshop CS6</small>" alt="Same Day Appliance Repair wanted a website that looked friendly and professional, provided tips for appliance maintenance and allowed online inquiries. It's due for an update in 2015.<br><small>samedayrepairgainesville.com<br>samedayrepairjax.com</small>
             "></a>
             <a href="img/images_gallery/b-01.png" id="fullscreen">
-            <img src="img/images_gallery/01.png" title="Space Movie for 352 Media Group<br><small></small>" alt="Mockup<br/>Adobe Fireworks CS5, Adobe Photoshop CS5<br/><br/>The first design project during my internship at 352 was to design a website for what inspires me. I chose space and movies, naturally leading to Space Movie.<br/><br/>The design was built around an image for the game Starcraft 2. I pulled colors from the image and used Impact as the decorative typeface.<br/><br/>Once the layout was decided upon, I used an existing image and pictures of a night-light in my apartment to create space ships firing at a driver.
+            <img src="img/images_gallery/01.png" title="Mock - Space Movie<br><small>Fireworks CS5, Photoshop CS5</small>" alt="The goal of this project from 352 was to mock a website for what inspires me. I thought of space and movies, so I put them together and created a site for a Space Movie. The design was built around an image from the game Starcraft 2.
             "></a>
             <a href="img/images_gallery/b-05.png" id="fullscreen">
-            <img src="img/images_gallery/05.png" title="Calvin and Hobbes Themed Responsive Layout for the Santa Fe College DMT Program<br><small></small>" alt="http://www.dustinaguilar.com/adapt<br/>Adobe Dreamweaver CS5, Notepad++, Firebug for Firefox, Adobe Photoshop CS5<br/><br/>Toward the end of the Digital Media Technology program at Santa Fe College, I built a responsive/adaptive layout. I used photographs of my Calvin and Hobbes collection as graphics.<br/><br/>Some interesting aspects were using macro shots of the comics to create depth in an otherwise 2D image, and using Hobbes' body to help eye flow.
+            <img src="img/images_gallery/05.png" title="Calvin and Hobbes Themed Responsive Layout<br><small>Dreamweaver CS5, Notepad++, Firebug, Photoshop CS5</small>" alt="I built this layout using tools in Dreamweaver. I used photographs of my Calvin and Hobbes collection for graphics. Currently, I use it as a base for extremely simple responsive layouts despite abandoning Dreamweaver as a tool.<br><small>http://www.dustinaguilar.com/adapt</small>
             " ></a>
             <a href="img/images_gallery/b-09.png" id="fullscreen">
-            <img src="img/images_gallery/09.png" title="Helvetica Article for the Santa Fe College DMT Program<br><small></small>" alt="Adobe Photoshop CS5, Adobe InDesign CS5<br/><br/>The goal was to design a magazine layout based on the font Helvetica.<br/><br/>NASA's Space Shuttle is a recognizable icon of the United States, so replacing 'United States' with 'Helvetica' gives us with a potent delivery of the article's subject. It also depicts the large and subtle role the typeface plays in our lives. The photo (credit unknown) was adjusted for color and the quote doubles as a lead-in to the actual article.
+            <img src="img/images_gallery/09.png" title="Helvetica Article<br><small>Photoshop CS5, InDesign CS5</small>" alt="The goal for this early school project was to design a magazine layout based on the font Helvetica. NASA's Space Shuttle is a recognizable American icon, so replacing 'United States' with 'Helvetica' seemed like a good choice for a potent delivery of the article's subject matter.
             "></a>
             <a href="img/images_gallery/b-12.png" id="fullscreen">
-            <img src="img/images_gallery/12.png" title="Business Card for Sterling Limousine's Ultimate Party Bus<br><small></small>" alt="Notepad ++, Firebug for Firefox, Adobe Photoshop CS5<br/><br/>Initial graphic work for Ultimate Party Bus -- Unfortunately, due to time constraints with school and work, we put the task of design in more capable hands while I did smaller, manageable projects and IT work so I could focus exclusively on school.
+            <img src="img/images_gallery/12.png" title="Ultimate Party Bus<br><small>Notepad ++, Firebug, Photoshop CS5</small>" alt="Initial graphic work for Ultimate Party Bus -- Unfortunately, due to time constraints with school and work, we put the task of design in more capable hands while I did IT work and smaller, manageable projects so I could focus on school.
             "></a>
             <a href="img/images_gallery/b-17.png" id="fullscreen">
-            <img src="img/images_gallery/17.png" title="Cerwin Vega magazine insert for the Santa Fe College DMT Program<br><small></small>" alt="Adobe Photoshop CS5, Printed<br/><br/>I created and printed a small (unofficial) ad for the speaker company Cerwin-Vega. The project took about 30 minutes from start to finish, which at that time was a milestone in trusting my own instincts.
+            <img src="img/images_gallery/17.png" title="Cerwin Vega Ad<br><small>Photoshop CS5, Printed</small>" alt="I created and printed this small (unofficial) ad for the speaker company Cerwin-Vega. The project took about 30 minutes from start to finish, which at that time signaled a milestone in trusting my own instincts.
             "></a>
             <!-- <a href="img/images_gallery/b-13.png" id="fullscreen">
             <img src="img/images_gallery/13.png" title="Windows 7 Customization" alt="Notepad++, Rainmeter, 3D Rendering in Photoshop<br/><br/>I use the open-sourced Rainmeter and stylesheets to customize my desktop and create effects such as the clock behind my logo. Several of the icons in the dock are custom as well, as is the entire Windows theme.
             "></a> -->
             <a href="img/images_gallery/b-04.png" id="fullscreen">
-            <img src="img/images_gallery/04.png" title="Character project for the Santa Fe College DMT Program<br><small></small>" alt=" Printed<br/><br/>Adobe Illustrator CS5<br/><br/>This poster depicts two fonts and two letters: Century Gothic and Courier with Y & X, respectively.<br/><br/>I deliberately ignored a 'rule of thumb' about placing text on top of text.  The broken rule went through the critique completely unnoticed (in printed form) and the design was regarded as a success.
+            <img src="img/images_gallery/04.png" title="Chars Project<br><small>Illustrator CS5, Printed</small>" alt="What makes this school project important to me is that I deliberately ignored a 'rule of thumb' about placing text on top of text. The broken rule went through the critique completely unnoticed (in printed form) and the design was regarded as a success. Text on top of text is a very common theme in my projects.
             "></a>
            <!--  <a href="img/images_gallery/b-07.png" id="fullscreen">
             <img src="img/images_gallery/07.png" title="Off Camber Mobile App for the Santa Fe College DMT Program" alt="Adobe Photoshop CS5, Uncoded<br/><br/>Off Camber Mobile is a tablet app that supplements the imaginary Off Camber Bicycle enthusiast website.<br/><br/>The photo swipes left/right, the icons can be pulled forward or pushed back and the headlines scroll up/down. This project also utilizes Layer Comps, which enable clients to navigate mockups within a single PDF."
             ></a> -->
             <a href="img/images_gallery/b-03.png" id="fullscreen">
-            <img src="img/images_gallery/03.png" title="Tee 4 Kids Classic for 352 Media Group<br><small></small>" alt="Adobe Fireworks CS5, Adobe Photoshop CS5<br/><br/>The assignment was to pick a charity event and design their website. I chose the 'Golf Tournament'.
+            <img src="img/images_gallery/03.png" title="Tee 4 Kids Classic<br><small>Fireworks CS5, Photoshop CS5</small>" alt="352 tasked me with picking a charity event and mocking a website design. I chose the 'Golf Tournament'.
             "></a>
             <a href="img/images_gallery/b-18.png" id="fullscreen">
-            <img src="img/images_gallery/18.png" title="Competition Finalist for Grooveshark and 352 Media Group<br><small></small>" alt="Adobe Illustrator CS5, Adobe Photoshop CS5<br/><br/>Playtime Toys was one of the design options for the Fall 2011 Web Design Competition held by 352 Media Group and Grooveshark. This design was a finalist in the competition and I still use elements from this project for mockup / layout design.
+            <img src="img/images_gallery/18.png" title="Web Design Competition for Grooveshark and 352<br><small>Illustrator CS5, Photoshop CS5</small>" alt="Playtime Toys was the theme I chose for the Fall 2011 Web Design Competition held by 352 and Grooveshark. It's purpose is to display potential layout patterns and UI styles as opposed to an actual website. It was a finalist in the competition and I still pull resources from it to this day.
             "></a>
-            <a href="img/images_gallery/b-10.png" id="fullscreen">
-            <img src="img/images_gallery/10.png" title="Planet Power radio station logo for the Digital Media Technology program at Santa Fe College<br><small></small>" alt="Adobe After Effects CS5, Adobe Illustrator CS5<br/><br/>In class, we collectively came up with elements that could possibly be in a radio station's logo: Globe, antenna, lightning, speakers and a shield.<br/><br/>I didn't want to use an actual shield because they make me think of Anti-Virus scams so I used a Flying V and a guitar pick to mimic the shape. I converted the center of the speaker to a globe, placed the antenna in the middle and used a lightning for the lightning. The metal texture of the letters comes from a penny.
-            "></a>
+            <!-- <a href="img/images_gallery/b-10.png" id="fullscreen">
+            <img src="img/images_gallery/10.png" title="Radio Station logo<br><small>Adobe After Effects CS5, Adobe Illustrator CS5</small>" alt="At school, we came up with elements that could possibly be in a radio station's logo: globe, antenna, lightning, speakers and a shield.<br/><br/>I didn't want to use a shield because they make me think of Anti-Virus scams so I used a Flying V and guitar pick to mimic the shape.
+            "></a> -->
             <a href="img/images_gallery/b-14.png" id="fullscreen">
-            <img src="img/images_gallery/14.png" title="Lighting and Perspective<br><small></small>" alt="Adobe Photoshop CS5<br/><br/>The goal here was to work on perspective, shadows and accurate lighting. Using a Mercedes-Benz photo, I replaced the cars with Sterling Limousines' H2 stretch and their Range Rover.
+            <img src="img/images_gallery/14.png" title="Lighting Project<br><small>Photoshop CS5</small>" alt="One night for fun, I decided to work on achieving accurate lighting. Using a Mercedes-Benz photo, I replaced the cars with Sterling Limousines' H2 stretch and their Range Rover.
             "></a>
             <a href="img/images_gallery/b-15.png" id="fullscreen">
-            <img src="img/images_gallery/15.png" title="Booyah Supersnacks<br><small></small>" alt="Adobe Illustrator CS5<br/><br/>Booyah Supersnacks was a fun Illustrator project I did in school that, for the most part, speaks for itself :)
+            <img src="img/images_gallery/15.png" title="Booyah Supersnacks<br><small>Illustrator CS5</small>" alt="Booyah Supersnacks was a fun Illustrator project I did in school that, for the most part, speaks for itself :)
             "></a>
            <!--  <a href="img/images_gallery/b-11.png" id="fullscreen">
             <img src="img/images_gallery/11.png" title="Planet Power Splash" alt="Go to the next slide to view the graphic in motion!<br/>Adobe After Effects CS5, Adobe Photoshop CS5, Vimeo<br/><br/>This school project was part of the semester-long campaign that used the title 'Planet Power'. The goal was to create a computer generated video lead-in / splash.
@@ -129,8 +129,8 @@
         </script>
         </div>
       </div>
-		</section>
-	
+        </section>
+    
     <footer>
       <div class="row">
         <div class="large-12 columns">
@@ -140,7 +140,7 @@
           <small>Copyright 1999-2015 | Dustin Aguilar</small>
         </div>
           <!-- <a href="http://www.behance.net/dustinaguilar"><img src="img/_ico-behance.png"></a>
-  			  <a href="http://www.linkedin.com/pub/dustin-aguilar/33/703/50b"><img src="img/_ico-linkedin.png"></a> -->
+              <a href="http://www.linkedin.com/pub/dustin-aguilar/33/703/50b"><img src="img/_ico-linkedin.png"></a> -->
       </div>
     </footer>
 
