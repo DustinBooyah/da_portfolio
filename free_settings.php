0c3936e3b68768c179888c11a9d327c2984b590d
Dustin Aguilar <dustin@dustinaguilar.com>
2015-08-18 05:23:49 -0400

    Update from the server
    I manually updated the website not too long ago. This brings in those
    changes.

--- free_settings.php
+++ free_settings.php
@@ -1,7 +1,7 @@
-<?php
-
-$email_to = "eightball007@hotmail.com"; // your email address
-$email_subject = "Message sent from Website"; // email subject line
-$thankyou = "thankyou.htm"; // thank you page
-
-?>
+<?php
+
+$email_to = "eightball007@hotmail.com"; // your email address
+$email_subject = "Message sent from Website"; // email subject line
+$thankyou = "thankyou.htm"; // thank you page
+
+?>
